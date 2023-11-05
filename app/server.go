edc7018177ea2f20e335eb5e74a6c5e1778b51c3
Cameron Brill <c00brill@gmail.com>
2023-11-05 11:57:04 -0500

    fix: continually handle connections

--- app/server.go
+++ app/server.go
@@ -9,25 +9,32 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
+	fmt.Println("starting server")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer func() {
+		err = l.Close()
+		if err != nil {
+			fmt.Println("closing listener: ", err.Error())
+			os.Exit(1)
+		}
+	}()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	err = http.HandleConnection(conn)
-	if err != nil {
-		fmt.Println("handling connection: ", err.Error())
-		os.Exit(1)
+		err = http.HandleConnection(conn)
+		if err != nil {
+			fmt.Println("handling connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 
 }
